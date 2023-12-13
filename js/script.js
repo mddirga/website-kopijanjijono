762ce102eaec42cc8db8ffbfc2bb97a620e02365
madedirga22 <madedirga22@gmail.com>
2023-12-13 11:35:53 +0800

    Update Search Form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle Class Active
+// Toggle Class Active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // Jika Hamburger Menu diKlik
@@ -6,10 +6,25 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle Class Active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+}
+
+// Klik di luar element
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  
 });
