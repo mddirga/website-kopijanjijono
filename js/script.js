5d273fd7fcf1987365b6ac456d68e5906d193c18
madedirga22 <madedirga22@gmail.com>
2023-12-15 10:10:16 +0800

    Shopping Cart Section

--- js/script.js
+++ js/script.js
@@ -14,17 +14,31 @@ document.querySelector('#search-button').onclick = (e) => {
   searchForm.classList.toggle('active');
   searchBox.focus();
   e.preventDefault();
+};
+
+// Toggle Class Active untuk Shopping Cart
+const shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+
 }
 
 // Klik di luar element
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button')
+
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
   }
   
 });
